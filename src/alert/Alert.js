d05b33d2fef612537d9d327d228444ccabbcc2e1
Tyson Louth <tysonlouth@gmail.com>
2021-12-13 08:51:16 -0500

    Testing eslint

--- src/alert/Alert.js
+++ src/alert/Alert.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 import { StyledAlert, StyledIcon } from './Alert.styles';
 
-const Alertd = (props) => {
+const Alertdasdas = (props) => {
   const { variant, icon, size, children, className, dataTestId } = props;
 
   return (
