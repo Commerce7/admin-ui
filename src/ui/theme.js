d491f8f2c6510acc3e5c9fe8f28b91c8f1d24855
Tyson Louth <tysonlouth@gmail.com>
2022-02-09 09:41:35 -0500

    Added box shadow and media query to global theme

--- src/ui/theme.js
+++ src/ui/theme.js
@@ -25,6 +25,11 @@ const borderColors = {
   light: 'rgb(224 224 224)'
 };
 
+const boxShadow = {
+  light: '2px 4px 6px rgba(0,0,0,0.15)',
+  dark: '2px 4px 6px rgba(0,0,0,0.5)'
+};
+
 export const createTheme = (mode) => ({
   c7__ui: {
     mode,
@@ -38,7 +43,17 @@ export const createTheme = (mode) => ({
     backgroundColor: backgroundColors[mode],
     secondaryBackgroundColor: secondaryBackgroundColors[mode],
     borderColor: borderColors[mode],
-    borderRadius: '3px'
+    borderRadius: '3px',
+    boxShadow: boxShadow[mode],
+    breakpoints: {
+      smallUp: `only screen`,
+      smallOnly: `only screen and (max-width: 39.9375em)`,
+      mediumUp: `only screen and (min-width: 40em)`,
+      mediumOnly: `only screen and (min-width: 40em) and (max-width: 79.9375em)`,
+      largeUp: `only screen and (min-width: 80em)`,
+      largeOnly: `only screen and (min-width: 80em) and (max-width: 89.9375em)`,
+      xLargeUp: `only screen and (min-width: 90em)`
+    }
   }
 });
 
