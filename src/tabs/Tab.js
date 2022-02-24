4bed289ff8bb757f15a63d1cfb158887e573e381
Jacob Hildebrandt <jacob@commerce7.com>
2022-02-23 16:22:17 -0800

    Added PropTypes and Defaults

--- src/tabs/Tab.js
+++ src/tabs/Tab.js
@@ -1,9 +1,10 @@
+import PropTypes from 'prop-types';
 import { TabStyles } from './Tabs.styles';
 
 const Tab = (props) => {
   const {
     children,
-    activeClassName = 'active',
+    activeClassName,
     className,
     component,
     onClick,
@@ -44,6 +45,55 @@ const Tab = (props) => {
       {children}
     </TabStyles>
   );
+};
+
+Tab.defaultProps = {
+  children: null,
+  activeClassName: 'active',
+  className: null,
+  component: null,
+  onClick: null,
+  href: null,
+  dataTestId: null
+};
+
+Tab.propTypes = {
+  /**
+   * The content of the component.
+   */
+  children: PropTypes.node,
+
+  /**
+   * Add className to the outermost element
+   */
+  className: PropTypes.string,
+
+  /**
+   * Add test attribute to the element. Used internally for testing.
+   */
+  dataTestId: PropTypes.string,
+
+  /**
+   * The component to render as a replacement for `<a/>`.
+   * This can be used to pass in components from client-side routing libraries like react-router.
+   * If this property is set, all other props will be passed to the component.
+   */
+  component: PropTypes.any, // eslint-disable-line
+
+  /**
+   * Callback fired when the component is clicked.
+   */
+  onClick: PropTypes.func,
+
+  /**
+   * The url to link to.
+   */
+  href: PropTypes.string,
+
+  /**
+   * The active className to mark the component as active. Styling targets this classname.
+   */
+  activeClassName: PropTypes.node
 };
 
 export default Tab;
