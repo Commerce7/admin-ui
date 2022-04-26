6c42c8c5a44bfce840a1f4146272d419a732435b
Tyson Louth <tysonlouth@gmail.com>
2022-04-26 10:01:17 -0400

    Updated modal footer css

--- src/modal/ModalFooter.styles.js
+++ src/modal/ModalFooter.styles.js
@@ -5,10 +5,23 @@ import { colors } from './theme';
 const StyledModalFooter = styled.div`
   padding: 20px;
   display: flex;
+  flex-wrap: wrap;
   justify-content: flex-end;
+  gap: 10px;
 
   background-color: ${({ theme }) => colors[theme.c7__ui.mode].backgroundColor};
   border-top: 1px solid ${({ theme }) => colors[theme.c7__ui.mode].borderColor};
+
+  button,
+  a {
+    margin-left: 0px !important;
+    flex: 1;
+    ${({ theme }) => `
+      @media ${theme.c7__ui.breakpoints.mediumUp} {
+        flex: none;
+      }
+    `}
+  }
 `;
 
 export { StyledModalFooter };
