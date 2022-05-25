4003d3a2b3abfe65e58afaf4289611d302143c5e
Jacob Hildebrandt <jacob@commerce7.com>
2022-05-25 14:56:17 -0700

    Added Spinner component

---
+++ src/spinner/Spinner.styles.js
@@ -0,0 +1,27 @@
+import styled, { keyframes } from 'styled-components';
+
+import Icon from '../icon';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg); 
+  }
+`;
+
+const StyledSpinner = styled.div`
+  text-align: center;
+  padding: 30px 0;
+`;
+
+const StyledIcon = styled(Icon)`
+  animation: ${rotate} 2s linear infinite;
+  width: 24px;
+  height: 24px;
+  min-width: 24px;
+  min-height: 24px;
+`;
+
+export { StyledSpinner, StyledIcon };
