238a6886dc9c46bb0616aceae9163d62484348d2
Jacob Hildebrandt <jacob@commerce7.com>
2023-03-17 13:18:48 -0700

    Add level 4 headings

--- src/heading/Heading.stories.js
+++ src/heading/Heading.stories.js
@@ -5,6 +5,7 @@ export const Basic = () => (
     <Heading level={1}>Heading 1</Heading>
     <Heading level={2}>Heading 2</Heading>
     <Heading level={3}>Heading 3</Heading>
+    <Heading level={4}>Heading 4</Heading>
   </>
 );
 
