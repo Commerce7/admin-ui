60a9e3d8a2033c1eadcf6514c0fd522edc581096
Yalcin Tatar <yalcintatar41@gmail.com>
2023-04-23 00:37:52 -0700

    upgrade to 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,19 @@
 module.exports = {
-  core: {
-    builder: 'webpack5'
-  },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-postcss'
-  ]
+    '@storybook/addon-postcss',
+    '@storybook/addon-mdx-gfm'
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  // {/* we can't use dymaic title in v2 I disabled store v2 to use dynamic title to show  => https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#mdx2-upgrade */}
+  features: { storyStoreV7: false },
+  docs: {
+    autodocs: true
+  },
+  staticDirs: ['../static']
 };
