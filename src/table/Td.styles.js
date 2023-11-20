85dc835930de9f61603484fd9497f23cc07e5942
Andrea Dyck <7304753+andreadyck@users.noreply.github.com>
2023-11-20 14:29:32 -0800

    Update table padding, heading font weight, tweak avatar sizes and tag padding

--- src/table/Td.styles.js
+++ src/table/Td.styles.js
@@ -2,7 +2,15 @@ import styled from 'styled-components';
 
 const StyledTd = styled.td`
   text-align: ${({ align }) => align};
-  padding: 15px;
+  padding: 15px 7px;
+
+  &:first-of-type {
+    padding-left: 15px;
+  }
+
+  &:last-of-type {
+    padding-right: 15px;
+  }
 `;
 
 export { StyledTd };
--- src/table/Tfoot.styles.js
+++ src/table/Tfoot.styles.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components';
 
-import { tfootColors } from './theme';
+import { tfootColors, theadColors } from './theme';
 
 const StyledTfoot = styled.tfoot`
   width: 100%;
 
   tr {
     background-color: ${({ theme }) =>
-      tfootColors[theme.c7__ui.mode].backgroundColor} !important;
+      theadColors[theme.c7__ui.mode].backgroundColor} !important;
+    border-top: 1px solid
+      ${({ theme }) => tfootColors[theme.c7__ui.mode].borderColor};
   }
 
   td {
