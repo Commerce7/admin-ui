0edc3900c2d9be3a6071efdf7edc8bd8a43653e0
Felipe Soares <felipeluizsoares@gmail.com>
2025-05-26 12:24:22 -0700

    fix: adjust auto generated code docs

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,13 @@ import StoriesWrapper from '../src/stories/StoriesWrapper';
 
 export default {
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' }
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    docs: {
+      source: {
+        type: 'dynamic',
+        excludeDecorators: true
+      }
+    }
   }
 };
 
